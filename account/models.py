ad788be0c496d13afe43a9e284cf76b7b49a798e
amal-dev-01 <devmpamal@gmail.com>
2024-08-17 10:56:14 +0530

    first commit

---
+++ account/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
